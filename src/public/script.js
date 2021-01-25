362de5fdd8c83be281eb54e4cf5b1759ef780d86
Jayden Koh Jia Jian <66878523+jaydenkoh@users.noreply.github.com>
2021-01-25 13:31:10 +0800

    Update script.js

--- src/public/script.js
+++ src/public/script.js
@@ -1,5 +1,5 @@
 const clientDetails = require('https://raw.githubusercontent.com/jaydenkoh/multipurpose-bot-2/main/src/events/ready.js')
 
 function infotodisplay() {
-    document.getElementById("printedinfo").innerHTML = clientDetails
+    document.getElementById("printedinfo").innerHTML = clientDetails;
 }
